016001539996598bf42588a66f8a9464f7ae3f9b
BojanJakic <bojanjakic1@gmail.com>
2017-07-20 14:38:41 +0200

    add new <li>

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,8 +9,9 @@ const Home = (props) => {
             <Header/>
             <ul>
                 <li className="ul-title"><h4>HELLO USER !</h4></li>
-                <li>NEW CONTACT - Save new contact in local storage</li>
-                <li>CONTACT LIST - See all contact saved in local storage</li>
+                <li>NEW CONTACT - Save contact in local storage</li>
+                <li>CONTACT LIST - See contacts saved in local storage</li>
+                <li>SOURCE CODE - Published on GitHub</li>
             </ul>
             <div className="center-image">
                 <img src={'images.jpg'} className="img-responsive img-home" alt=""/>
