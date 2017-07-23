459cdaf6d171076245905eeedc48a94e40c1f57b
BojanJakic <bojanjakic1@gmail.com>
2017-07-23 15:44:24 +0200

    Use contact object inside state, add mergeStateValue(evt)

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Notification from './Notification'
 import {addContact, editContact, deleteContact} from '../js/contactCRUD.js'
 import {isFirstNameValid, isLastNameValid, isEmailValid, isFormValid, isPhoneValid} from '../js/customValidator.js'
+import update from 'immutability-helper';
 import '../style/ContactForm.css';
 
 class ContactForm extends Component {
@@ -9,11 +10,13 @@ class ContactForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: "",
-            firstName: "",
-            lastName: "",
-            email: "",
-            phone: "",
+            contact: {
+                id: "",
+                firstName: "",
+                lastName: "",
+                email: "",
+                phone: ""
+            },
             isFirstNameValid: false,
             isLastNameValid: false,
             isEmailValid: false,
@@ -24,47 +27,48 @@ class ContactForm extends Component {
 
     componentWillReceiveProps() {
         if (this.props.initInputValue !== undefined) {
-            this.setState({
-                id: this.props.initInputValue.id,
-                firstName: this.props.initInputValue.firstName,
-                lastName: this.props.initInputValue.lastName,
-                email: this.props.initInputValue.email,
-                phone: this.props.initInputValue.phone,
-            })
+            this.setState({contact:this.props.initInputValue})
         }
 
-        if(this.props.buttonAction === 'DELETE'){
+        if (this.props.buttonAction === 'DELETE') {
             var inputs = this.getAllInputs();
 
-            for(var i = 0; i < inputs.length; i++){
+            for (var i = 0; i < inputs.length; i++) {
                 inputs[i].setAttribute('readonly', 'readonly');
             }
         }
     }
 
     onChangeFirstNameHandler = (evt) => {
-        console.log(evt.target.value)
-        this.setState({firstName: evt.target.value});
+        this.mergeNewStateValue(evt);
         var isValid = isFirstNameValid(evt.target);
-        this.setState({isFirstNameValid : isValid})
+        this.setState({isFirstNameValid: isValid})
     };
 
     onChangeLastNameHandler = (evt) => {
-        this.setState({lastName: evt.target.value});
+        this.mergeNewStateValue(evt);
         var isValid = isLastNameValid(evt.target);
-        this.setState({isLastNameValid : isValid})
+        this.setState({isLastNameValid: isValid})
     };
 
     onChangeEmailHandler = (evt) => {
-        this.setState({email: evt.target.value});
+        this.mergeNewStateValue(evt);
         var isValid = isEmailValid(evt.target);
-        this.setState({isEmailValid : isValid})
+        this.setState({isEmailValid: isValid})
     };
 
     onChangePhoneHandler = (evt) => {
-        this.setState({phone : evt.target.value});
+        this.mergeNewStateValue(evt);
         var isValid = isPhoneValid(evt.target);
-        this.setState({isPhoneValid : isValid});
+        this.setState({isPhoneValid: isValid});
+    };
+
+    mergeNewStateValue = (evt) => {
+        var newState = update(this.state, {
+            contact: {[evt.target.name]: {$set: evt.target.value}}
+        });
+
+        this.setState(newState)
     };
 
     onSubmitHandler = (evt) => {
@@ -86,20 +90,13 @@ class ContactForm extends Component {
     };
 
     submitAction = () => {
-        var contact = {
-            id: this.state.id,
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            phone: this.state.phone
-        };
 
         if (this.props.buttonAction === 'DELETE') {
-            deleteContact(contact)
+            deleteContact(this.state.contact)
         } else if (this.props.buttonAction === 'EDIT') {
-            editContact(contact)
+            editContact(this.state.contact)
         } else {
-            addContact(contact)
+            addContact(this.state.contact)
         }
 
         this.cleanUp()
@@ -109,7 +106,7 @@ class ContactForm extends Component {
         var action = document.getElementsByClassName('submit-btn')[0].innerText;
         if (action === 'EDIT' || action === 'DELETE') {
             this.props.reloadContactList();
-        }else if(action === 'SAVE'){
+        } else if (action === 'SAVE') {
             this.resetForm();
             this.notification.prepareNotification(action);
         }
@@ -119,18 +116,20 @@ class ContactForm extends Component {
 
     deleteClass = () => {
         var inputs = this.getAllInputs();
-        for(var i =0; i < inputs.length; i++){
+        for (var i = 0; i < inputs.length; i++) {
             inputs[i].parentElement.classList.remove('success');
         }
     };
 
     resetForm = () => {
         this.setState({
-            id: "",
-            firstName: "",
-            lastName: "",
-            email: "",
-            phone: "",
+            contact: {
+                id: "",
+                firstName: "",
+                lastName: "",
+                email: "",
+                phone: ""
+            },
             isFirstNameValid: false,
             isLastNameValid: false,
             isEmailValid: false,
@@ -152,62 +151,63 @@ class ContactForm extends Component {
                 <Notification ref={notification => this.notification = notification}/>
                 <div className="row">
                     <div className="col-xs-2 col-sm-2 col-md-3 col-lg-3"></div>
-                <div className="col-xs-8 col-sm-8 col-md-6 col-lg-6">
-                    <div className="form-header">Contact Form</div>
-                    <form id="contactForm" onSubmit={(evt) => this.onSubmitHandler(evt)} autoComplete="off">
-                        <div className="form-group">
-                            <label>First Name :</label>
-                            <div className='input-group'>
-                                <div className="input-group-addon">
-                                    <span className="fa fa-user-circle-o"></span>
+                    <div className="col-xs-8 col-sm-8 col-md-6 col-lg-6">
+                        <div className="form-header">Contact Form</div>
+                        <form id="contactForm" onSubmit={(evt) => this.onSubmitHandler(evt)} autoComplete="off">
+                            <div className="form-group">
+                                <label>First Name :</label>
+                                <div className='input-group'>
+                                    <div className="input-group-addon">
+                                        <span className="fa fa-user-circle-o"></span>
+                                    </div>
+                                    <input id="a" value={this.state.contact.firstName} className="form-control"
+                                           type="text"
+                                           name="firstName" onChange={(evt) => this.onChangeFirstNameHandler(evt)}/>
                                 </div>
-                                <input id="a" value={this.state.firstName} className="form-control" type="text"
-                                       name="firstName" onChange={(evt) => this.onChangeFirstNameHandler(evt)}/>
+                                <span className="error-message" id="firstName"></span>
                             </div>
-                            <span className="error-message" id="firstName"></span>
-                        </div>
-                        <div className="form-group">
-                            <label>Last Name :</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <span className="fa fa-user-circle-o"></span>
+                            <div className="form-group">
+                                <label>Last Name :</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <span className="fa fa-user-circle-o"></span>
+                                    </div>
+                                    <input value={this.state.contact.lastName} className="form-control" type="text"
+                                           name="lastName" onChange={(evt) => this.onChangeLastNameHandler(evt)}/>
                                 </div>
-                                <input value={this.state.lastName} className="form-control" type="text"
-                                       name="lastName" onChange={(evt) => this.onChangeLastNameHandler(evt)}/>
+                                <span className="error-message" id="lastName"></span>
                             </div>
-                            <span className="error-message" id="lastName"></span>
-                        </div>
-                        <div className="form-group">
-                            <label>Email :</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <span className="fa fa-envelope-o"></span>
+                            <div className="form-group">
+                                <label>Email :</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <span className="fa fa-envelope-o"></span>
+                                    </div>
+                                    <input value={this.state.contact.email} className="form-control" type="text"
+                                           name="email" onChange={(evt) => this.onChangeEmailHandler(evt)}/>
                                 </div>
-                                <input value={this.state.email} className="form-control" type="text"
-                                       name="email" onChange={(evt) => this.onChangeEmailHandler(evt)}/>
+                                <span className="error-message" id="email"></span>
                             </div>
-                            <span className="error-message" id="email"></span>
-                        </div>
-                        <div className="form-group">
-                            <label>Phone :</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <span className="fa fa-phone-square"></span>
+                            <div className="form-group">
+                                <label>Phone :</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <span className="fa fa-phone-square"></span>
+                                    </div>
+                                    <input value={this.state.contact.phone} className="form-control" type="text"
+                                           name="phone" onChange={(evt) => this.onChangePhoneHandler(evt)}/>
                                 </div>
-                                <input value={this.state.phone} className="form-control" type="text"
-                                       name="phone" onChange={(evt) => this.onChangePhoneHandler(evt)}/>
+                                <span className="error-message" id="phone"></span>
                             </div>
-                            <span className="error-message" id="phone"></span>
-                        </div>
 
-                        <div className="form-group">
-                            <button type="submit"
-                                    className="btn btn-primary btn-responsive submit-btn">{buttonTitle}</button>
-                        </div>
-                    </form>
+                            <div className="form-group">
+                                <button type="submit"
+                                        className="btn btn-primary btn-responsive submit-btn">{buttonTitle}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-xs-2 col-sm-2 col-md-3 col-lg-3"></div>
                 </div>
-                <div className="col-xs-2 col-sm-2 col-md-3 col-lg-3"></div>
-            </div>
             </div>
         )
     }
